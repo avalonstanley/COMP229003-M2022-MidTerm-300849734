cc905a50f69d8e5145ed7f61f7a77f9e9c03c97b
avalonstanley <avalonstanley@hotmail.com>
2022-07-06 14:06:29 -0400

    Final commit

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 // Do not expose your credentials in your code.
-let atlasDB = "mongodb+srv://<username>:<passoword>@<cluster>/carstore?retryWrites=true&w=majority";
+let atlasDB = "mongodb+srv://avalonstanley:xLOB2p75ZwfUM4we@cluster001.3oynn.mongodb.net/Inventory?retryWrites=true&w=majority";
 
 // Database setup
 let mongoose = require('mongoose');
--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,14 @@ let usersController = require('../controllers/user');
 let passport = require('passport');
 
 // Routes for sign-up
-router.get('/signup', usersController.renderSignup);
-router.post('/signup', usersController.signup);
+router.get('/signup', usersController.renderSignup);//render
+router.post('/signup', usersController.signup);//process
 
 // Routes for sign-in
-router.get('/signin', usersController.renderSignin);
-router.post('/signin', usersController.signin);
+router.get('/signin', usersController.renderSignin);//render
+router.post('/signin', usersController.signin);//process
 
 // Route for sign-out
-router.get('/signout', usersController.signout);
+router.get('/signout', usersController.signout);//render
 
 module.exports = router;
